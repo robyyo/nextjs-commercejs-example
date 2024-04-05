da0fd828fc8bc2252cb66f405d8131464ae8c9c7
Rob <me@robertfinkley.com>
2024-04-04 21:14:38 -0400

    created product filter component

--- components/ProductFilter.jsx
+++ components/ProductFilter.jsx
@@ -1,15 +1,52 @@
 'use client'
+import { useState } from 'react'
 import ProductsGrid from './ProductsGrid'
 import ActiveFilters from './ActiveFilter'
 import Filters from './Filters'
 
-const ProductFilter = ({ filters, activeFilters, sortOptions, products }) => {
+const ProductFilter = ({ sortOptions, products, categories }) => {
+  let addCheckedToCategories = categories.map((category) => {
+    return { ...category, checked: false }
+  })
+
+  const [updatedCategories, setUpdatedCategories] = useState(
+    addCheckedToCategories,
+  )
+
+  let activeFilters = updatedCategories.filter((category) => category.checked)
+
+  const handleChangeFilter = (id) => {
+    const newFilters = updatedCategories.map((filter) => {
+      if (filter.id === id) {
+        return { ...filter, checked: !filter.checked }
+      }
+      return filter
+    })
+    setUpdatedCategories(newFilters)
+  }
+
+  let filteredProducts = products.filter((product) => {
+    let activeCategoryList = activeFilters.map((category) => category.slug)
+    if (activeCategoryList.length === 0) return true
+    return product.categories.some((category) => {
+      return activeCategoryList.includes(category.slug)
+    })
+  })
+
   return (
     <div className="bg-gray-50">
       <section aria-labelledby="filter-heading">
-        <Filters filters={filters} sortOptions={sortOptions} />
-        <ActiveFilters activeFilters={activeFilters} />
-        <ProductsGrid products={products} />
+        <Filters
+          sortOptions={sortOptions}
+          categories={updatedCategories}
+          handleChangeFilter={handleChangeFilter}
+          activeFilters={activeFilters}
+        />
+        <ActiveFilters
+          activeFilters={activeFilters}
+          handleChangeFilter={handleChangeFilter}
+        />
+        <ProductsGrid products={filteredProducts} />
       </section>
     </div>
   )
